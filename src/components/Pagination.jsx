a032ead43f54055e2fa4b1fcca1735a36676bfdf
clstyn <fiorenzacelestyn@gmail.com>
2024-06-08 23:23:19 +0700

    integrate kalori

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -10,11 +10,16 @@ export default function PaginationControls({
     console.log(currPage);
   }, [currPage]);
 
+  const pagesShown = 5;
+
   const renderPageNumbers = () => {
     const pagNumbers = [];
 
-    for (let i = 1; i <= totalPages; i++) {
+    for (let i = currPage; i <= totalPages; i++) {
       pagNumbers.push(i);
+      if (pagNumbers.length === pagesShown) {
+        break;
+      }
     }
 
     return pagNumbers.map((num) => (
@@ -31,24 +36,29 @@ export default function PaginationControls({
   };
 
   return (
-    <div className="flex items-center justify-center mt-5 text-birdong">
-      <button
-        disabled={currPage === 1}
-        onClick={() => handlePagination(currPage - 1)}
-        className="disabled:text-gray-300 disabled:cursor-not-allowed"
-      >
-        <NavigateBefore style={{ fontSize: "32px" }}></NavigateBefore>
-      </button>
+    <>
+      <div className="flex items-center justify-center mt-5 text-birdong">
+        <button
+          disabled={currPage === 1}
+          onClick={() => handlePagination(currPage - 1)}
+          className="disabled:text-gray-300 disabled:cursor-not-allowed"
+        >
+          <NavigateBefore style={{ fontSize: "32px" }}></NavigateBefore>
+        </button>
 
-      <div className="flex gap-2">{renderPageNumbers()}</div>
+        <div className="flex gap-2">{renderPageNumbers()}</div>
 
-      <button
-        disabled={currPage === totalPages}
-        onClick={() => handlePagination(currPage + 1)}
-        className="disabled:text-gray-300 disabled:cursor-not-allowed"
-      >
-        <NavigateNext style={{ fontSize: "32px" }}></NavigateNext>
-      </button>
-    </div>
+        <button
+          disabled={currPage === totalPages}
+          onClick={() => handlePagination(currPage + 1)}
+          className="disabled:text-gray-300 disabled:cursor-not-allowed"
+        >
+          <NavigateNext style={{ fontSize: "32px" }}></NavigateNext>
+        </button>
+      </div>
+      <p className="text-center font-medium mt-2">
+        Total halaman: {totalPages}
+      </p>
+    </>
   );
 }
